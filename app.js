402ec3bb8e1a921659e925061cdd0f3bcdfbb9c1
עדי יעקבי <adijak@auth.ad.bgu.ac.il>
2019-05-21 21:15:11 +0300

    hey

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var DButilsAzure = require('./DButils');
 
-DButilsAzure.execQuery("INSERT INTO names (name) VALUES ('adi')")
 
 var port = 3000;
 app.listen(port, function () {
@@ -10,20 +9,22 @@ app.listen(port, function () {
 });
 
 //Example
-app.get('/select', function(req, res){
-    DButilsAzure.execQuery("SELECT * FROM names")
-    .then(function(result){
-        res.send(result)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
-})
+// app.get('/select', function(req, res){
+//     DButilsAzure.execQuery("SELECT * FROM names")
+//     .then(function(result){
+//         res.send(result)
+//     })
+//     .catch(function(err){
+//         console.log(err)
+//         res.send(err)
+//     })
+// })
+
 
 app.get('/getPoint', function(req, res){
-    var pointName = req.pointName;
-    DButilsAzure.execQuery("SELECT top (2) p.[description], p.[rank], p.[numofviews] , r.[review] FROM [points] p inner join [reviews] r on p.[id]=r.[pointid] where p.[name] = " + pointName + " order by r.[date] ASC ")
+    let pointName = req.query.pointName;
+    console.log("**************" + pointName);
+    DButilsAzure.execQuery("SELECT top (2) p.[description], p.[rank], p.[numofviews] , r.[review] FROM [points] p inner join [reviews] r on p.[id]=r.[pointid] where p.[name] = '" + pointName + "' order by r.[date] ASC ")
     .then(function(result){
         res.send(result)
     })
