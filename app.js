30357ee84157cfa660c32259eb568b13dbe1cabf
עדי יעקבי <adijak@auth.ad.bgu.ac.il>
2019-05-21 20:16:44 +0300

    hey

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.listen(port, function () {
     console.log('Example app listening on port ' + port);
 });
 
+//Example
 app.get('/select', function(req, res){
     DButilsAzure.execQuery("SELECT * FROM names")
     .then(function(result){
@@ -20,3 +21,14 @@ app.get('/select', function(req, res){
     })
 })
 
+app.get('/getPoint', function(req, res){
+    var pointName = req.pointName;
+    DButilsAzure.execQuery("SELECT top (2) p.[description], p.[rank], p.[numofviews] , r.[review] FROM [points] p inner join [reviews] r on p.[id]=r.[pointid] where p.[name] = " + pointName + " order by r.[date] ASC ")
+    .then(function(result){
+        res.send(result)
+    })
+    .catch(function(err){
+        console.log(err)
+        res.send(err)
+    })
+})
