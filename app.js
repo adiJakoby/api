82788e40351b9488257051457cb092c4cdde8590
עדי יעקבי <adijak@auth.ad.bgu.ac.il>
2019-05-22 11:48:58 +0300

    hey

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(require('body-parser').json())
 
 app.get('/getPoint', function(req, res){
     let pointName = req.query.pointName;
-    console.log("**************" + pointName);
+    // console.log("**************" + pointName);
     DButilsAzure.execQuery("SELECT top (2) p.[description], p.[rank], p.[numofviews] , r.[review] FROM [points] p inner join [reviews] r on p.[id]=r.[pointid] where p.[name] = '" + pointName + "' order by r.[date] ASC ")
     .then(function(result){
         res.send(result)
@@ -41,9 +41,25 @@ app.get('/getPoint', function(req, res){
 app.post('/logIn', function(req, res){
     let userName = req.body.userName;
     let password = req.body.password;
-    //console.log("**************" + pointName);
+    // console.log("**************" + userName + "   " + password);
     DButilsAzure.execQuery(
         "IF ( SELECT COUNT (*) FROM [dbo].[Passwords] P WHERE P.[USERNAME] = '" + userName + "' AND P.[PASSWORD] = HASHBYTES('SHA2_512','" + password + "')) > 0 SELECT 1 ELSE SELECT 0 ")
+    .then(function(result){
+        res.send(result)
+    })
+    .catch(function(err){
+        console.log(err)
+        res.send(err)
+    })
+})
+
+//restore password
+app.post('/restorePassword', function(req, res){
+    let userName = req.body.userName;
+    let question = req.body.question;
+    let answer = req.body.answer;
+    DButilsAzure.execQuery(
+        "IF ( SELECT COUNT (*) FROM [dbo].[Questions] Q WHERE Q.[USERNAME] = '" + userName + "' AND Q.[QUESTION] = '" + question + "' AND Q.[ANSWER] = '" + answer + "') = 1 SELECT * FROM [dbo].[Passwords] P WHERE P.[USERNAME] = username")
     .then(function(result){
         res.send(result)
     })
